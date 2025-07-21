aefa27bc5c817959320a5b99e91ec4fa86cf9026
salismazaya <salismazaya@gmail.com>
2025-07-21 11:40:06 +0700

    ✨ feat(chess): add chess game functionality with react-chessboard and chess.js ✨ feat(audio): add move sound effect for chess pieces ♻️ refactor(Gameplay): replace placeholder with interactive chess board component 📦 chore(package.json): add chess.js and react-chessboard dependencies

--- src/pages/Gameplay.tsx
+++ src/pages/Gameplay.tsx
@@ -1,9 +1,36 @@
-const Gameplay = () => {
-    return (
-        <h1 className="text-2xl font-semibold">
-         Gameplay
-        </h1>
-    )
-}
+import { useRef } from "react";
+import Board from "../components/ui/common/board/Board";
+import { Chess } from "chess.js";
 
-export default Gameplay
+const Gameplay = () => {
+  const chessGameRef = useRef(new Chess());
+  const chessGame = chessGameRef.current;
+
+  const handleSelfMove = async ({
+    from_position,
+    to_position,
+    piece,
+  }: {
+    from_position: string;
+    to_position: string;
+    piece: string;
+  }) => {
+    console.log({ from_position, to_position, piece });
+    new Audio("/audio/move.mp3").play();
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl font-semibold">Gameplay</h1>
+      <div className="max-w-xl">
+        <Board
+          boardOrientation="white"
+          chess={chessGame}
+          onSelfMove={handleSelfMove}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Gameplay;
