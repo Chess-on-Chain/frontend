fa17e4a1bdc7ec7b09af702d64eca1d538675328
heryfirman <herriifirmansyah9999@gmail.com>
2025-07-23 14:31:03 +0700

    Update(Page): Profile V2 & Card-History

--- src/pages/ProfileSettings.tsx
+++ src/pages/ProfileSettings.tsx
@@ -1,4 +1,5 @@
 import { Camera, ChevronDown } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const ProfileSettings = () => {
   return (
@@ -7,8 +8,8 @@ const ProfileSettings = () => {
 
       {/* TOP Section */}
       <div className="flex items-center gap-6 mb-10">
-        <div className="relative w-24 h-24 rounded-full bg-secondary">
-          <div className="absolute bottom-0 right-0 w-7 h-7 rounded-full bg-gray-500 flex items-center justify-center shadow-md">
+        <div className="relative w-24 h-24 rounded-full bg-linear-to-b from-secondary to-[#4F3B2B]">
+          <div className="absolute bottom-0 right-0 w-7 h-7 rounded-full bg-accent flex items-center justify-center shadow-md">
             <Camera size={16} className="text-black" />
           </div>
         </div>
@@ -16,18 +17,19 @@ const ProfileSettings = () => {
         <div>
           <div className="flex items-center gap-2 text-lg sm:text-xl lg:text-2xl font-semibold">
             @Username{" "}
+            <span className="w-2 h-2 translate-y-0.5 bg-white/50 rounded-full" />
             <span className=" mt-1.5 text-sm sm:text-base lg:text-lg text-gray-400">
               {" "}
               IDN
             </span>
           </div>
-          <div className="text-gray-400">
+          <div className="lg:text-xl text-gray-400">
             Score: <span className="text-white">100</span>
           </div>
-          <div className="flex items-center gap-1 mt-3 w-fit text-sm lg:text-base">
+          <div className="flex items-center gap-3 mt-3 w-fit text-sm lg:text-base">
             <span className="text-white font-semibold">Rank: </span>
-            <span className="px-2 py-0 5 font-bold w-fit text-sm lg:text-xl bg-secondary text-black rounded-sm">
-              100
+            <span className="px-2 py-0 5 font-bold w-fit text-sm lg:text-xl bg-accent text-black rounded-sm">
+              500
             </span>
           </div>
         </div>
@@ -48,12 +50,13 @@ const ProfileSettings = () => {
                 type="text"
                 name="usrname"
                 id="username"
-                className="w-full px-3 py-2 border border-white/20 text-white bg-black rounded"
+                className="w-full px-3 py-2 text-lg tracking-wide border border-white/20 text-white bg-black rounded disabled:bg-primary disabled:border-white/5"
                 defaultValue="Firman"
+                disabled
               />
               <button
                 type="button"
-                className="px-4 py-2 rounded-sm bg-secondary text-black font-semibold hover:bg-secondary"
+                className="px-4 py-2 rounded-sm bg-secondary text-black font-semibold hover:bg-secondary cursor-not-allowed"
               >
                 Change
               </button>
@@ -73,7 +76,7 @@ const ProfileSettings = () => {
                 type="text"
                 name="firstName"
                 id="firstName"
-                className="w-full px-3 py-2 text-white bg-black border border-white/20 rounded"
+                className="w-full px-3 py-2 text-lg tracking-wide text-white bg-black border border-white/20 rounded"
               />
             </div>
           </div>
@@ -87,7 +90,7 @@ const ProfileSettings = () => {
                 type="text"
                 name="lastName"
                 id="lastName"
-                className="w-full px-3 py-2 text-white bg-black border border-white/20 rounded"
+                className="w-full px-3 py-2 text-lg tracking-wide text-white bg-black border border-white/20 rounded"
               />
             </div>
           </div>
@@ -100,7 +103,7 @@ const ProfileSettings = () => {
               <select
                 name="country"
                 id="country"
-                className="w-full px-3 py-2 border border-white/20 bg-black text-white appearance-none"
+                className="w-full px-3 py-2 text-lg tracking-wide border border-white/20 bg-black text-white appearance-none"
                 defaultValue="Indonesia"
               >
                 <option value="Indonesia">INDONESIA</option>
@@ -119,13 +122,15 @@ const ProfileSettings = () => {
           <div className="flex space-x-6 mt-10 py-2 items-center justify-center lg:justify-start">
             <button
               type="submit"
-              className="px-6 py-2 font-semibold text-white bg-transparen border border-secondary hover:bg-secondary rounded cursor-pointer"
+              className="px-6 py-2 text-base lg:text-xl font-semibold text-white bg-transparen border border-white/20 hover:bg-secondary rounded cursor-pointer"
             >
-              Cancel
+              <Link to="/profile">
+                Cancel
+              </Link>
             </button>
             <button
               type="submit"
-              className="px-6 py-2 font-semibold text-black bg-secondary hover:bg-secondary rounded cursor-pointer"
+              className="px-6 py-2 text-base lg:text-xl font-semibold text-black bg-secondary hover:bg-secondary rounded cursor-pointer"
             >
               Save All
             </button>
