657b3174de29e49a67723cbe7fbe483198cd64d2
heryfirman <herriifirmansyah9999@gmail.com>
2025-07-25 14:42:57 +0700

    Remove(Move-History): in desktop hidden & in mobile remove hardcode

--- src/pages/Gameplay.tsx
+++ src/pages/Gameplay.tsx
@@ -412,7 +412,7 @@ const MobileLayout: React.FC<LayoutProps> = ({ handleSelfMove }) => {
       {/* RESIGN BUTTON */}
       <div className="flex justify-center items-center w-full my-8">
         <button
-          className="text-center block"
+          className="text-center block cursor-pointer"
           onClick={() => {
             actor.resign();
           }}
@@ -424,40 +424,6 @@ const MobileLayout: React.FC<LayoutProps> = ({ handleSelfMove }) => {
           </div>
           <p className="text-white">Resign</p>
         </button>
-      </div>
-
-      {/* SCORE BOTTOM */}
-      <div className="w-full border-t border-white/20 text-center text-sm text-white">
-        {/* MOVES */}
-        <div className="flex-1 flex justify-center h-[400px] w-full py-4">
-          <div className="w-full lg:hidden h-full">
-            <div className="flex items-center justify-center p-4 h-full">
-              <div className="border border-white/40 rounded w-full h-full flex flex-col">
-                {/* Scrollable content */}
-                <div className="flex-1 overflow-y-auto hide-scrollbar">
-                  {Array.from({ length: 30 }, (_, i) => (
-                    <div
-                      key={i}
-                      className="grid grid-cols-[auto_1fr_1fr] divide-x-3 divide-dashed divide-white/20 items-center text-white text-sm border-b border-white/20 py-2 px-4"
-                    >
-                      <span className="min-w-10 text-left text-white/40">
-                        {i + 1}
-                      </span>
-                      <span className="text-center">♟ C6</span>
-                      <span className="text-center">♟ C6</span>
-                    </div>
-                  ))}
-                </div>
-
-                {/* Fixed bottom */}
-                <div className="h-16 border-t border-white/20 px-4 flex items-center text-white text-sm text-wrap lg:text-base space-x-4">
-                  <p className="font-medium">Score : </p>
-                  <span className="align-baseline">0 | 1</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
@@ -472,7 +438,7 @@ const DesktopLayout: React.FC<LayoutProps> = ({ handleSelfMove }) => {
       <div className="flex-1 h-full flex justify-center">
         <div className="flex justify-center items-center w-full lg:w-3/5 h-full">
           <button
-            className="mx-auto text-center block"
+            className="mx-auto text-center block cursor-pointer"
             onClick={() => {
               actor && actor.resign();
             }}
@@ -496,7 +462,7 @@ const DesktopLayout: React.FC<LayoutProps> = ({ handleSelfMove }) => {
       </div>
 
       {/* RIGHT - MOVES & SCORE */}
-      <div className="flex-1 flex justify-center h-full py-4">
+      <div className="opacity-0 flex-1 flex justify-center h-full py-4">
         <div className="w-full xl:w-3/5 h-full">
           <div className="flex items-center justify-center p-4 h-full">
             <div className="border border-white/40 rounded w-full h-full flex flex-col">
